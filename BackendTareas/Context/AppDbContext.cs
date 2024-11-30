cc1f4007f3108b2958fa58e9e38523e46de0fef1
David Diaz <sinedindnd1@gmail.com>
2024-11-30 13:32:41 -0500

    Inicio del proyecto tareas api rest con .net 8

---
+++ BackendTareas/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendTareas.Context
+{
+    public class AppDbContext: DbContext
+    {
+
+    }
+}
---
+++ BackendTareas/Models/Tarea.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendTareas.Models
+{
+    public class Tarea
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public Tarea() 
+        {
+            CreateAt = DateTime.Now;
+        }
+
+    }
+}
